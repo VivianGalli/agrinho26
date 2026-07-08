d7dfd8b158dca84ac04278d63145ed2f65dfdf4d
VivianGalli <vivian.madruga@escola.pr.gov.br>
2026-07-08 08:29:41 -0300

    e

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ botao1.addEventListener("click", function () {
         botao1.textContent = "Mostrar resposta";
     }
 });
+
+const botao2 = document.getElementById("botao2");
+const resposta2 = document.getElementById("resposta2");
+
+botao2.addEventListener("click", function () {
+    if (resposta2.style.display === "none") {
+        resposta2.style.display = "block";
+        botao2.textContent = "Esconder resposta";
+    } else {
+        resposta2.style.display = "none";
+        botao2.textContent = "Mostrar resposta";
+    }
+});
+
+const botao3 = document.getElementById("botao3");
+const resposta3 = document.getElementById("resposta3");
+
+botao3.addEventListener("click", function () {
+    if (resposta3.style.display === "none") {
+        resposta3.style.display = "block";
+        botao3.textContent = "Esconder resposta";
+    } else {
+        resposta3.style.display = "none";
+        botao3.textContent = "Mostrar resposta";
+    }
+});
+
+const botao4 = document.getElementById("botao4");
+const resposta4 = document.getElementById("resposta4");
+
+botao4.addEventListener("click", function () {
+    if (resposta4.style.display === "none") {
+        resposta4.style.display = "block";
+        botao4.textContent = "Esconder resposta";
+    } else {
+        resposta4.style.display = "none";
+        botao4.textContent = "Mostrar resposta";
+    }
+});
